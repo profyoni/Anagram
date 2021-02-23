b50c99e1d8ba3be102699313e59e33809e47501e
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-02-23 11:33:00 -0500

    Initial commit

---
+++ src/edu/touro/cs/Temp.java
@@ -0,0 +1,76 @@
+package edu.touro.cs;
+
+import java.util.*;
+
+// TODO : Replace this file with your code
+class AnagrammedWord implements Comparable<AnagrammedWord>
+{
+    String word;
+    String canonForm;
+
+    @Override
+    public int compareTo(AnagrammedWord that) {
+        int cmp = this.canonForm.compareTo(that.canonForm);
+        if (cmp != 0)
+        return cmp;
+
+        cmp = this.word.compareToIgnoreCase(that.word);
+        return cmp;
+    }
+}
+
+public class Temp {
+    public static String canonicalForm(String s)
+    {
+        char [] letters = s.replace(" ","").
+                toUpperCase().toCharArray();
+        Arrays.sort( letters );
+        return new String(letters);
+    }
+
+    public static List< List<String> > FindAllAnagrams(List<String> wordList)
+    {
+        if (wordList == null)
+            throw new NullPointerException("wordList is null");
+        List< List<String> > ret = new ArrayList<>();
+        List<AnagrammedWord> aList = new ArrayList<>();
+        for(String s: wordList)
+        {
+
+            if (s == null)
+                throw new NullPointerException("wordList contains a null value");
+            AnagrammedWord aw = new AnagrammedWord();
+            aw.word = s;
+            aw.canonForm = canonicalForm(s);
+            aList.add(aw);
+        }
+        // sort by canonForm, word
+        Collections.sort(aList);
+        String previousCf = "";
+        List<String> anagramList = null;
+        for(AnagrammedWord aw : aList)
+        {
+            if ( ! aw.canonForm.equals(previousCf))
+            {
+                previousCf = aw.canonForm;
+                if (anagramList != null && anagramList.size() > 1)
+                    ret.add(anagramList);
+                // new CF
+                anagramList = new ArrayList<>();
+            }
+            anagramList.add( aw.word );
+        }
+        if (anagramList.size() > 1)
+            ret.add(anagramList);
+
+        Collections.sort(ret, new ListComp());
+        return ret;
+    }
+
+    private static class ListComp implements Comparator<List<String> >{
+        @Override
+        public int compare(List<String> o1, List<String> o2) {
+            return o1.get(0).compareTo(o2.get(0));
+        }
+    }
+}
